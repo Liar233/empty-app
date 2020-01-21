1a3940c1cf81f506d1c059185c2226ccc2cf52e9
Sitnikov Dmitriy <d.sitnikov@ibolit.pro>
2020-01-21 12:41:35 +0300

    reform empty app.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,7 @@
 package main
 
-import "github.com/Liar233/empty-app/internal/app/definition"
+import "github.com/Liar233/empty-app/internal/app"
 
 func main() {
-	application := definition.InitApp()
-	application.Start()
+	println(app.Hello)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,36 +1,3 @@
 package app
 
-import (
-	"os"
-	"os/signal"
-	"syscall"
-	"fmt"
-)
-
-type Application struct {
-	stop chan bool
-}
-
-func (app *Application) Start() {
-	signs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
-
-	signal.Notify(signs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		<-signs
-		fmt.Println()
-		fmt.Println("Stopping...")
-		done <- true
-	}()
-
-	<-done
-
-	fmt.Println("Exit")
-}
-
-func NewApplication() Application {
-	return Application{
-		stop: make(chan bool),
-	}
-}
+const Hello = "Hello"
